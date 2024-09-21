587619b9ea81ab12fe85c22d1bc0bc0c1199d73d
SURAJ_KUMAR <surajk38656@gmail.com>
2024-09-21 16:56:43 +0530

    ReactTest

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,24 @@
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
 const Home = () =>{
+    const navigate = useNavigate();
+    const counterApp = () =>{
+        navigate("/counterapp")
+    }
+    const themeApp = () =>{
+        navigate("/themeapp")
+    }
+    const todoApp = () =>{
+        navigate("/todolist")
+    }
     return(
         <>
         <center><h1>Home Page</h1></center>
-        <div id="hero">
+        <div id="home">
+            <Button variant="outline-primary" onClick={counterApp}>Counter App</Button>
+            <Button variant="outline-primary" onClick={themeApp}>Theme App</Button>
+            <Button variant="outline-primary" onClick={todoApp}>ToDo App</Button>
         </div>
         </>
     )
